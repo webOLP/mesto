3d709d1b490872f06079e495e1649e58df4c8541
OLP <semtara@yandex.ru>
2022-04-09 19:53:39 +0300

    sdad

---
+++ scripts/popup-edit.js
@@ -0,0 +1,26 @@
+// const openEditProfileButton = document.querySelector('.profile__add-button');
+// const editProfilePopup = document.querySelector('.popup-edit');
+// const closeEditProfileButton = editProfilePopup.querySelector('.popup-edit__close-button');
+// console.log(openEditProfileButton);
+// console.log(editProfilePopup);
+// console.log(closeEditProfileButton);
+
+
+
+
+// function toggleEditPopup() {
+    
+// console.log('in before');
+//     editProfilePopup.classList.add('popup-edit_is-opened');
+    
+// console.log('in after');
+// }
+
+// console.log('out');
+// toggleEditPopup();
+
+
+
+
+// console.log('1');
+// console.log('2');

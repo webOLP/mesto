28e8cf7518f3430896bee364a986612b630b13b4
OLP <semtara@yandex.ru>
2022-06-27 22:40:18 +0300

    feat: buttons activated

--- scripts/popup-add.js
+++ scripts/popup-add.js
@@ -4,6 +4,9 @@ const closeAddPlacePopupButton = addPlacePopup.querySelector('.popup__close-butt
 let placeNamePopup = addPlacePopup.querySelectorAll('.form__input')[0];
 let placeLinkPopup = addPlacePopup.querySelectorAll('.form__input')[1];
 let placesBox = document.querySelector('.places');
+const places = document.querySelector('.places');
+const likeButtons = places.querySelectorAll('.places__like-button');
+const deleteButtons = places.querySelectorAll('.places__delete-button');
 const initialPlaces = [
     {
         name: 'Архыз',
@@ -31,9 +34,17 @@ const initialPlaces = [
     }
 ];
 
-initialPlaces.forEach(function(place){
-    addPlace(place.name,place.link)
-});
+function likeButtonListener(button) {
+    button.addEventListener('click', (evt) => evt.currentTarget.classList.toggle('places__like-button_active')) 
+}    
+
+/*Вероятно, лучше не использовать и стрелочные функции и функциональные выражения 
+в одном коде, чтобы не было путаницы, но мне было интересно опробовать на деле новые знания*/
+
+function deleteButtonListener(button){
+    button.addEventListener('click', (evt) => evt.currentTarget.closest('.places__place').remove())
+}
+
 
 function openPopup() {
     addPlacePopup.classList.add('popup_is-opened');
@@ -53,7 +64,10 @@ function popupOverlayClick(evt) {
 
 function savePopup(evt) {
     evt.preventDefault();
-    addPlace(placeNamePopup.value, placeLinkPopup.value);
+    let placeCard = addPlace(placeNamePopup.value, placeLinkPopup.value);
+    likeButtonListener(placeCard.querySelector('.places__like-button'));
+    deleteButtonListener(placeCard.querySelector('.places__delete-button'));
+    placesBox.prepend(placeCard);
     closePopup();
 }
 
@@ -62,10 +76,15 @@ function addPlace(nameValue, linkValue) {
     const placeContainer = placeTemplate.querySelector('.places__place').cloneNode(true);
     placeContainer.querySelector('.places__image').src = `${linkValue}`;
     placeContainer.querySelector('.places__title').textContent = `${nameValue}`;
-    placesBox.prepend(placeContainer);
+    return placeContainer
+    
+   
 }
 
-
+initialPlaces.forEach(function(place){
+    let placeCard = addPlace(place.name,place.link)
+    placesBox.prepend(placeCard);
+});
 openAddPlaceButton.addEventListener('click', openPopup);
 closeAddPlacePopupButton.addEventListener('click', closePopup);
 addPlacePopup.addEventListener('click', popupOverlayClick)
